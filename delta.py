e33759e76db5f22a0709d8c073ac86eeecf882df
KirKur <aridavortex90>
2017-07-17 11:58:15 +0300

    Python 3+ version print fix

--- delta.py
+++ delta.py
@@ -12,7 +12,6 @@ array2 = []
 with open('deltas\delta.txt') as img_text:
     for string in img_text:
         col1, col2 = re.split(r'\s{3}\b', string, 1)
-        print(col1, '__', col2)
         array1.append(remove_sp(col1))
         array2.append(remove_sp(col2))
 
@@ -25,18 +24,18 @@ ptr1, ptr2 = 0, 0
 
 while ptr1 < len(array1) and ptr2 < len(array2):
     if not array1[ptr1] == array2[ptr2]:
-        if array1[ptr1+1] == array2[ptr2]:
+        if array1[ptr1+1] == array2[ptr2]:  # addition in first column
             delta1.write(bytes.fromhex(array1[ptr1]))
             ptr1 += 1
-        elif array1[ptr1] == array2[ptr2+1]:
+        elif array2[ptr2+1] == array1[ptr1]:  # addition in second column
             delta2.write(bytes.fromhex(array2[ptr2]))
             ptr2 += 1
-        else:
+        else:  # addition in both columns
             delta1.write(bytes.fromhex(array1[ptr1]))
             delta2.write(bytes.fromhex(array2[ptr2]))
             ptr1 += 1
             ptr2 += 1
-    else:
+    else:  # no additions
         delta3.write(bytes.fromhex(array2[ptr2]))
         ptr1 += 1
         ptr2 += 1
